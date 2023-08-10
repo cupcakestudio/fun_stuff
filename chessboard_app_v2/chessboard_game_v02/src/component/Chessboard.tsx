ceff5c22829deeacf91bfb148955bab11054dd67
Ting Hua Lin <coolting.lin3@gmail.com>
2023-08-10 14:07:14 +0200

    rendered colored tiles and correct number/letter pos

--- chessboard_app_v2/chessboard_game_v02/src/component/Chessboard.tsx
+++ chessboard_app_v2/chessboard_game_v02/src/component/Chessboard.tsx
@@ -11,10 +11,19 @@ function Chessboard(){
  let board = [];
 
  //letterTiles for loop, render tiles so that the numbers and letters reads correct, from top left to bottom right
+for (let j = NumberTiles.length-1; j >= 0; j--) //numbertiles so that it renders one number with diff letters first.
+ {
+  for (let i = 0; i < LetterTiles.length; i++) {
+    //add number and position index to give a tile a color, result of index: even  = black, odd = white.
+    const numberIndex = j+i + 2 // +2 to start from count number 2 (sinxe 1 + 1 = 2)
 
- for (let i = 0; i < LetterTiles.length; i++){
-  for (let j = NumberTiles.length-1; j >= 0; j++) {
-    board.push(<span>{LetterTiles[i]}{NumberTiles[j]} </span>) //renders the axisis, span needs to be written exactly like that, otherwise the numbering looks weird
+    //check if numberIndex is modulus 2.
+    if (numberIndex % 2 === 0){
+      board.push(<div className='tile_black'>{LetterTiles[i]}{NumberTiles[j]} </div>) //renders the axisis, span needs to be written exactly like that, otherwise the numbering looks weird
+    } else {
+      board.push(<div className='tile_white'>{LetterTiles[i]}{NumberTiles[j]} </div>) //renders the axisis, span needs to be written exactly like that, otherwise the numbering looks weird
+    }
+   
   }
  }
   
